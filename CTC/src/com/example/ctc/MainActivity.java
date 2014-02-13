673e68974c74607b75347fc2109b683735171f33
Gent <Gent@MyLaptop>
2014-02-14 00:56:53 +0700

    add question page

--- CTC/src/com/example/ctc/MainActivity.java
+++ CTC/src/com/example/ctc/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Checkable;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.RadioButton;
@@ -19,11 +20,8 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private int age;
-	private int height;
-	private int weight;
-	private boolean sex;
 	private boolean have = false;
+	
 	RadioGroup radioGroup4;
 	RadioButton radioButton1;
 	RadioButton radioButton0;
@@ -36,22 +34,26 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 		radioGroup4 = (RadioGroup) findViewById(R.id.radioGroup4);
 		radioButton1 = (RadioButton) findViewById(R.id.radio1);
 		radioButton0 = (RadioButton) findViewById(R.id.radio0);
 
 		OnClickListener radioClickListener = new OnClickListener() {
 
+			
+			
 			public void onClick(View v) {
 				if (v.getId() == radioGroup4.getCheckedRadioButtonId() && have) {
 					radioGroup4.clearCheck();
 				} else {
 					have = true;
 				}
+				
+				
 			}
 		};
-
+		
 		OnCheckedChangeListener radioCheckChangeListener = new OnCheckedChangeListener() {
 
 			@Override
@@ -68,13 +70,6 @@ public class MainActivity extends Activity {
 		radioButton1.setOnClickListener(radioClickListener);
 		radioButton0.setOnClickListener(radioClickListener);	
 		
-		if(radioButton0.isChecked()){
-			sex = false;
-		}
-		if(radioButton1.isChecked()) {
-			sex = true;
-		}
-		
 		//read string in text box
 		editText1 = (TextView) findViewById(R.id.editText1);
 		editText2 = (TextView) findViewById(R.id.editText2);
@@ -90,11 +85,16 @@ public class MainActivity extends Activity {
 				localBundle.putString("Age", editText1.getText().toString());
 				localBundle.putString("Height", editText2.getText().toString());
 				localBundle.putString("Weight", editText3.getText().toString());
+				if(radioButton0.isChecked()){
+					localBundle.putBoolean("Sex", false);}
+				else if (radioButton1.isChecked()){
+					localBundle.putBoolean("Sex", true);
+				}
 				Intent localIntent = new Intent(MainActivity.this, ResultActivity.class);
 				localIntent.putExtras(localBundle);
 				startActivity(localIntent);
 			
-				finish();
+				//finish();
 
 			}
 		});
@@ -107,12 +107,5 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public boolean setSex(boolean sex){
-		return this.sex = sex;
-	}
-	
-	public boolean getSex(){
-		return sex;
-	}
 
 }
--- CTC/src/com/example/ctc/ResultActivity.java
+++ CTC/src/com/example/ctc/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.ctc;
 
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.app.Activity;
@@ -20,14 +21,14 @@ public class ResultActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
 		
-		MainActivity sv = new MainActivity();
-		boolean sex = sv.getSex();
 		
-		
+		//recieve value from main page
 		String a = getIntent().getStringExtra("Age");
 		String h = getIntent().getStringExtra("Height");
 		String w = getIntent().getStringExtra("Weight");
+		boolean sex = getIntent().getBooleanExtra("Sex", false);
 		
+		//chang string to integer type
 		double age = Integer.parseInt(a);
 		double height = Integer.parseInt(h);
 		double weight = Integer.parseInt(w);
@@ -45,16 +46,29 @@ public class ResultActivity extends Activity {
 			result = 66.47 + (13.75 * weight) + (5 * height) - (6.76 * age);
 		}
 		
+		//show the result
 		TextView text_TotalCalorie = (TextView) findViewById(R.id.textView1);
-		text_TotalCalorie.setText("" + sex + " Kcal/day");
+		text_TotalCalorie.setText("" + result + " Kcal/day");
 		
-		
+		//back to main page
 		final Button btn_back = (Button) findViewById(R.id.button1);
 		btn_back.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent newActivity = new Intent(ResultActivity.this,
 						MainActivity.class);
 				startActivity(newActivity);
+				finish();
+
+			}
+		});
+		
+		//for temporaly
+		final Button btn_food = (Button) findViewById(R.id.button2);
+		btn_food.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Uri uri = Uri.parse("http://www.calorieking.com/foods/");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri); 
+				startActivity(intent);
 				finish();
 
 			}
